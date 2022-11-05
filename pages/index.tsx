8b0290c4d4b2b74b2b6609ddcf970c07f4ce890e
mateen kazia <mateen.kazia@dws.la>
2022-11-04 21:52:55 -0500

    Adding simple layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+// import Image from 'next/image';
+import ParticlesContainer from '../src/components/Particles';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 export default function Home() {
   return (
@@ -10,7 +14,23 @@ export default function Home() {
         <meta name='description' content='Welcome~!' />
         <link rel='icon' href='/favicon.png' />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <ParticlesContainer />
+        <Typography variant='h1'>Welcome to my website!</Typography>
+        {/* <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 500,
+              height: 500
+            }
+          }}
+        >
+          <Paper elevation={3} />
+        </Box> */}
+      </main>
 
       <footer className={styles.footer}></footer>
     </div>
